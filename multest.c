1e54048822b08711907b27ccf77ee0188a8c9bd2
edgar ramos <edgar.rene.ramos.acosta@uabc.edu.mx>
2023-02-05 20:12:56 -0800

    Regions are added to organize the entire work

--- multest.c
+++ multest.c
@@ -11,78 +11,51 @@ Version: 1.0, 02-02-2023
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-
-
-
-
-
-
+#include <curses.h>   
 
 const int MAX_SIZE= 512;
 
-char *findArgument(int argc, char *argv[], char *rank_cmd) {
-  for (int i = 0; i < argc; i++) {
-    if (strcmp(argv[i], rank_cmd) == 0) {
-      if (i + 1 < argc) {
-        return argv[i + 1];
-      } else {
-        return "";
-      }
-    }
-  }
-  return "";
-}
+void multiplyMatrices(float Matrix1[][MAX_SIZE], float Matrix2[][MAX_SIZE], float resultMatrix[][MAX_SIZE], int n);
+char *findArgument(int argc, char *argv[], char *cmd);
 
-void multiplyMatrices(float Matrix1[][MAX_SIZE], float Matrix2[][MAX_SIZE], float resultMatrix[][MAX_SIZE], int n) {
-    int i, j, k;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            resultMatrix[i][j] = 0;
-            for (k = 0; k < n; k++) {
-                resultMatrix[i][j] += Matrix1[i][k] * Matrix2[k][j];
-            }
-        }
-    }
-}
+
+
+
 
 int main(int argc, char *argv[]){
     float A[MAX_SIZE][MAX_SIZE], B[MAX_SIZE][MAX_SIZE], resultMatrix[MAX_SIZE][MAX_SIZE];
-    int n;
+    int n, blkNumber=0;
     int i, j,k;
     char *rank_cmd = "--rank";
+    char *block_cmd="--block-size";
+    
+#pragma region "arguments management"
 
-    printf("test");
-    //char *target = "--filename";
     char *rankNumber = findArgument(argc, argv, rank_cmd);
-  if (strlen(rankNumber) > 0) {
-    printf("rankNumber: %s\n", rankNumber);
-     n=atoi(rankNumber);
-  } else {
-    printf("rankNumber not found\n");
-  }
-    //int resultCompare=strcmp("-rank", argv[1]);
-/*
-    if (strcmp("-rank", argv[1])==0)
+    if (strlen(rankNumber) > 0) {
+        printf("rankNumber: %s\n", rankNumber);
+        n=atoi(rankNumber);
+    } else {
+        printf("rankNumber not found\n");
+    }
+    char *blockNumber = findArgument(argc, argv, block_cmd);
+    if (strlen(blockNumber) > 0) {
+        printf("blockNumber: %s\n", blockNumber);
+        blkNumber=atoi(blockNumber);
+    } else {
+        printf("blockNumber not found\n");
+    }
+    if(strcmp(argv[1],("--help"))==0)
     {
-        n=atoi(argv[2]);
+            printf("--help: Arguments: --rank ranknumber --block-size blkNumber");
 
     }
-    else if (strcmp("-rank", argv[3])==0)
-    {
-        n=atoi(argv[4]);
-        
-    }
-    else
-    {
 
-    }
-    
-  */
-    //printf("Enter the size of matrices: ");
-    //scanf("%d", &n);
 
-    
-    printf("Rank is %d. --Autofilling matrix with random numbers--\n",n);
+#pragma endregion
+
+#pragma region "Autofilling matrices with random float numbers"
+    printf("--Autofilling matrix with random numbers--\n");
     for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++) {
        
@@ -104,7 +77,8 @@ int main(int argc, char *argv[]){
             }
             printf("\n");
         }
-
+#pragma endregion 
+   
         multiplyMatrices(A, B, resultMatrix, n);
    
         printf("Product of matrices:\n");
@@ -117,4 +91,33 @@ int main(int argc, char *argv[]){
       
     return 0;
    
+}
+
+char *findArgument(int argc, char *argv[], char *cmd) {
+  for (int i = 0; i < argc; i++) {
+    if (strcmp(argv[i], cmd) == 0) {
+      if (i + 1 < argc) {
+        return argv[i + 1];
+      } else {
+          
+          
+            return "";
+            
+        
+      }
+    }
+  }
+  return "";
+}
+
+void multiplyMatrices(float Matrix1[][MAX_SIZE], float Matrix2[][MAX_SIZE], float resultMatrix[][MAX_SIZE], int n) {
+    int i, j, k;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            resultMatrix[i][j] = 0;
+            for (k = 0; k < n; k++) {
+                resultMatrix[i][j] += Matrix1[i][k] * Matrix2[k][j];
+            }
+        }
+    }
 }
